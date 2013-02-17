0e5956b72fbc0c178757c81587b01dc57ba84450
Joel <johnson.joel.86@gmail.com>
2013-02-16 20:02:00 -0700

    implementing ListIterator

--- src/main/java/com/joelj/collections/LinkedArrayBlockIterator.java
+++ src/main/java/com/joelj/collections/LinkedArrayBlockIterator.java
@@ -1,21 +1,24 @@
 package com.joelj.collections;
 
-import java.util.Iterator;
-import java.util.NoSuchElementException;
+import java.util.*;
 
 /**
  * User: Joel Johnson
  * Date: 2/16/13
  * Time: 4:25 PM
  */
-class LinkedArrayBlockIterator<T> implements Iterator<T> {
+class LinkedArrayBlockIterator<T> implements ListIterator<T> {
 	private LinkedArrayList.Block<T> current;
 	private int index;
+	private int globalIndex;
+	private Deque<LinkedArrayList.Block<T>> previousBlocks;
 
 	public LinkedArrayBlockIterator(LinkedArrayList.Block<T> block) {
 		assert block != null : "block can't be null";
 		current = block;
 		index = 0;
+		globalIndex = 0;
+		previousBlocks = new LinkedList<LinkedArrayList.Block<T>>();
 	}
 
 	@Override
@@ -30,6 +33,7 @@ class LinkedArrayBlockIterator<T> implements Iterator<T> {
 		}
 
 		if(index >= current.nextIndex) {
+			previousBlocks.push(current);
 			current = current.next;
 			while(current != null && current.getNextIndex() <= 0) {
 				current = current.next;
@@ -39,11 +43,58 @@ class LinkedArrayBlockIterator<T> implements Iterator<T> {
 		if(current == null) {
 			throw new NullPointerException("current is null. There is a bug in the hasNext method.");
 		}
-		return current.get(index++);
+
+		T result = current.get(index);
+		globalIndex++;
+		index++;
+		return result;
+	}
+
+	@Override
+	public boolean hasPrevious() {
+		return globalIndex > 0;
+	}
+
+	@Override
+	public T previous() {
+		if(globalIndex <= 0) {
+			throw new NoSuchElementException("Already at the beginning.");
+		}
+		if(index > 0) {
+			index--;
+			globalIndex--;
+			return current.get(index);
+		} else {
+			LinkedArrayList.Block<T> pop = previousBlocks.pop();
+			current = pop;
+			index = current.getNextIndex() - 1;
+			globalIndex--;
+			return current.get(current.getNextIndex());
+		}
+	}
+
+	@Override
+	public int nextIndex() {
+		return globalIndex + 1;
+	}
+
+	@Override
+	public int previousIndex() {
+		return globalIndex - 1;
 	}
 
 	@Override
 	public void remove() {
 		throw new UnsupportedOperationException("remove isn't implemented yet");
+	}
+
+	@Override
+	public void set(T t) {
+		current.array[index] = t;
+	}
+
+	@Override
+	public void add(T t) {
+		throw new UnsupportedOperationException("add isn't implemented yet");
 	}
 }

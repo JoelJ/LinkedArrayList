87f507feef0ec86121315a602503f8932bd93655
Joel <johnson.joel.86@gmail.com>
2013-02-16 19:04:13 -0700

    adding basic operations tests for the java.util.List.

--- src/test/java/com/joelj/collections/LinkedArrayListImplTest.java
+++ src/test/java/com/joelj/collections/LinkedArrayListImplTest.java
@@ -1,15 +1,38 @@
 package com.joelj.collections;
 
 import org.testng.annotations.Test;
+import static org.testng.Assert.*;
+
+import java.util.List;
 
 /**
  * User: Joel Johnson
  * Date: 2/16/13
  * Time: 6:44 PM
  */
+@SuppressWarnings("ForLoopReplaceableByForEach")
 public class LinkedArrayListImplTest {
 	@Test
-	public void testBasicOperations() throws Exception {
+	public void testBasicGet() throws Exception {
+		List<String> strings = LinkedArrayList.create();
+		for(int i = 0; i < 30; i++) {
+			strings.add(String.valueOf(i));
+		}
 
+		for(int i = 0; i < strings.size(); i++) {
+			String s = strings.get(i);
+			assertEquals(s, String.valueOf(i));
+		}
+	}
+
+	@Test
+	public void testBasicRemove() throws Exception {
+		List<String> strings = LinkedArrayList.create();
+		for(int i = 0; i < 30; i++) {
+			strings.add(String.valueOf(i));
+		}
+		strings.remove(15);
+		assertEquals(strings.get(14), "14");
+		assertEquals(strings.get(15), "16");
 	}
 }

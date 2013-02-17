c9db19c0d52a5d1760f5e896b9f20aaedc9fdf1d
Joel <johnson.joel.86@gmail.com>
2013-02-16 19:15:22 -0700

    Adding test to make sure new tests are added for methods that don't currently have an implementation.

--- src/test/java/com/joelj/collections/LinkedArrayListImplTest.java
+++ src/test/java/com/joelj/collections/LinkedArrayListImplTest.java
@@ -3,6 +3,7 @@ package com.joelj.collections;
 import org.testng.annotations.Test;
 import static org.testng.Assert.*;
 
+import java.util.Collections;
 import java.util.List;
 
 /**
@@ -34,5 +35,45 @@ public class LinkedArrayListImplTest {
 		strings.remove(15);
 		assertEquals(strings.get(14), "14");
 		assertEquals(strings.get(15), "16");
+	}
+
+	@Test
+	public void unsupportedMethods() {
+		List<Object> list = LinkedArrayList.create();
+
+		try {
+			list.addAll(0, Collections.emptyList());
+			fail("addAll(int, Collection) appears to have an implementation now. Remove this when there are tests that test it.");
+		} catch (UnsupportedOperationException ignore) {}
+
+		try {
+			list.retainAll(Collections.emptyList());
+			fail("retainAll(Collection) appears to have an implementation now. Remove this when there are tests that test it.");
+		} catch (UnsupportedOperationException ignore) {}
+
+		try {
+			list.add(0, new Object());
+			fail("add(int, E) appears to have an implementation now. Remove this when there are tests that test it.");
+		} catch (UnsupportedOperationException ignore) {}
+
+		try {
+			list.listIterator();
+			fail("listIterator() appears to have an implementation now. Remove this when there are tests that test it.");
+		} catch (UnsupportedOperationException ignore) {}
+
+		try {
+			list.listIterator(0);
+			fail("listIterator(int) appears to have an implementation now. Remove this when there are tests that test it.");
+		} catch (UnsupportedOperationException ignore) {}
+
+		try {
+			list.subList(0, 1);
+			fail("subList(int, int) appears to have an implementation now. Remove this when there are tests that test it.");
+		} catch (UnsupportedOperationException ignore) {}
+
+		try {
+			list.set(0, new Object());
+			fail("set(int, E) appears to have an implementation now. Remove this when there are tests that test it.");
+		} catch (UnsupportedOperationException ignore) {}
 	}
 }
